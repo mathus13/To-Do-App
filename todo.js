04145261fa656d39a037449968358f68160e84ce
HumanSprout <erikj.waters@gmail.com>
2016-01-22 19:35:05 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,25 +1,37 @@
 var inputText = document.getElementById("inputText"),
     todos = {},
     todoCounter = 0,
-    Todo = function (todoText, priority, key) {
-        var listItem = document.createElement("li");
-	    listItem.innerText = todoText;
-	    todoList.appendChild(listItem);
-    }
+    endCounter = 0
+
+listTodos = function () {
+  //lines 29 - 31 go here to keep it DRY
+  //for loop will run and call this function for priority first
+  //then run again for non priorities
+}
 
 inputText.onkeyup = function (event){
-	var todo_key, itemText = inputText.value;
+	var todo_key, itemText = inputText.value, i, todoText
 
   	if (undefined === itemText || itemText === "" || itemText === " ") {
  		return false;
     	}
     	if (event.which === 13) {
+    		    //if end counter is 0 add total objects from local storage
             todoCounter = todoCounter + 1;
-            todo_key = 'todo_' + todoCounter;          
-            //todos[todo_key].key = todo_key;
-            todos[todo_key] = new Todo(itemText, false, todo_key);
-                      
-            document.getElementById("inputText").blur();
+            endCounter = endCounter + 1;
+            todo_key = 'todo_' + todoCounter; 
+            console.log(todo_key);
+            todos[todo_key] = {};
+            todos[todo_key].key = todo_key;
+            todos[todo_key].listed = false;
+            todos[todo_key].todoText = itemText;
+            
+                //for (i)
+                //for in loop if listed = false && priority !null {
+                var listItem = document.createElement("li");
+	            listItem.innerText = todos[todo_key].todoText;
+	            todoList.appendChild(listItem);
+	            //todos[todo_key].listed = true;
             document.getElementById("inputText").value = "";
     }
 };
