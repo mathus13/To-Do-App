5dffe190c12289e199f5b7ecea0b89ebb98531ed
HumanSprout <erikj.waters@gmail.com>
2016-01-23 10:21:25 -0500

    less fluff
    need to start work on other functions now that this is working as intended, apparently all of my scope frustrations are solved by passing the first class objects that are out of scope to the upper level functions

--- todo.js
+++ todo.js
@@ -1,19 +1,21 @@
 var app = (function () {
 
-    var inputText = document.getElementById("inputText"), todos = {}, todoCounter = 0, endCounter = 0;
-    
+    var inputText = document.getElementById("inputText"), todos = {}, todoCounter = 0;
+    // uncomment when needed: endCounter = 0; 
+
+    doTheThing = function (todo_key, itemText) {
+    	todos[todo_key] = {};
+    	todos[todo_key].key = todo_key;
+    	//un-comment once loop is active, not needed atm
+    	//todos[todo_key].listed = false;
+    	todos[todo_key].todoText = itemText;
+    };
+
     listTodos = function (todo_key, todoText) {
         var listItem = document.createElement("li");
         listItem.innerText = todos[todo_key].todoText;
         todoList.appendChild(listItem);
     };
-
-    doTheThing = function (todo_key, itemText) {
-    	todos[todo_key] = {};
-    	todos[todo_key].key = todo_key;
-    	todos[todo_key].listed = false;
-    	todos[todo_key].todoText = itemText;
-    }
 
     inputText.onkeyup = function (event){
             var todo_key, itemText = inputText.value, i, todoText;
@@ -22,12 +24,12 @@ var app = (function () {
             return false;
     	    }
     	    if (event.which === 13) {
-    		        //if end counter is 0 add total objects from local storage
+    		    //if end counter is 0 add total objects from local storage
                 todoCounter = todoCounter + 1;
-                endCounter = endCounter + 1;
+                //uncomment when needed: endCounter = endCounter + 1;
                 todo_key = 'todo_' + todoCounter; 
                 console.log(todo_key);
-                doTheThing(todo_key,itemText);
+                doTheThing(todo_key, itemText);
                 listTodos(todo_key, todoText);
                     //for (i)
                     //for in loop if listed = false && priority !null {
@@ -35,7 +37,9 @@ var app = (function () {
                 document.getElementById("inputText").value = "";
         };
     };
-    return {
-    listTodos: listTodos
-    };
+    //do I even need to return this?
+    //return {
+    //listTodos: listTodos,
+    //todos: todos
+    //};
 } ());
