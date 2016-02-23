6a051cc41370cf2031489112ffe865c890bf7664
HumanSprout <erikj.waters@gmail.com>
2016-02-22 19:02:23 -0500

    changed todo counter assignment
    more preliminary work for rekey function, updated element creation function for single var declaration

--- todo.js
+++ todo.js
@@ -1,6 +1,12 @@
 var app = (function () {
-    var  targetKey, targetId, targetSpan, targetLi, todoCounter, reorder, splitKey, inputText = document.getElementById("inputText"), todos = {}, todoPriority = 0;
+    var  targetKey, targetId, targetSpan, targetLi, todoCounter, reorder, inputText = document.getElementById("inputText"),
+        todos = {},
+        todoPriority = 0;
     //creates the todo items
+    updateCounter = function () {
+        todoCounter = Object.keys(todos).length;
+        return todoCounter;
+    }
     itsTimeToMakeTheDonuts = function (todo_key, itemText) {
         todos[todo_key] = {};
         todos[todo_key].key = todo_key;
@@ -8,46 +14,47 @@ var app = (function () {
         todos[todo_key].priority = 0;
         todos[todo_key].todoText = itemText;
     };
-    //creates the dom elements
+    //creates the dom elements and assignes properties
     doTheThing = function (todo_key, todoText) {
-        //listItem
-        var listItem = document.createElement("li");
+        //create elements
+        var listItem = document.createElement("li"),
+            select = document.createElement("select"),
+            selectOpt0 = document.createElement("option"),
+            selectOpt1 = document.createElement("option"),
+            selectOpt2 = document.createElement("option"),
+            selectOpt3 = document.createElement("option"),
+            delButton = document.createElement("button"),
+            checkbox = document.createElement("input"),
+            textSpan = document.createElement("span");
+        //listItem properties
         listItem.dataset.key = todo_key;
         listItem.className = "list-item_default";
         listItem.id = "listItem_"+todo_key;
         todos[todo_key].listed = true;
-        //select for priority level
-        var select = document.createElement("select");
+        //(priority level)select properties
         select.dataset.key = todo_key;
         select.className = "select";
         select.id = "select_"+todo_key;
-        var selectOpt0 = document.createElement("option")
         selectOpt0.value = 0;
         selectOpt0.innerText = 0;
-        var selectOpt1 = document.createElement("option")
         selectOpt1.innerText = 1;
-        var selectOpt2 = document.createElement("option")
         selectOpt2.innerText = 2;
-        var selectOpt3 = document.createElement("option")
         selectOpt3.innerText = 3;
-        //delete button
-        var delButton = document.createElement("button");
+        //(delete) button properties
         delButton.dataset.key = todo_key;
         delButton.className = "button_delete";
         delButton.id = "delButton_"+todo_key;
-        //completed checkbox
-        var checkbox = document.createElement("input")
+        //(completed) checkbox properties
         checkbox.dataset.key = todo_key;
         checkbox.type = "checkbox";
         checkbox.className = "checkbox";
         checkbox.id = "checkbox_"+todo_key;
-        //span for text
-        var textSpan = document.createElement("span");
+        //(to do text) span properties
         textSpan.dataset.key = todo_key;
         textSpan.className = "span_todo-text-default";
         textSpan.id = 'span_'+todo_key;
         textSpan.innerText = todos[todo_key].todoText;
-        //append elements
+        //append elements 
         todoList.appendChild(listItem);
         listItem.appendChild(delButton);
         listItem.appendChild(checkbox);
@@ -59,18 +66,25 @@ var app = (function () {
         listItem.appendChild(textSpan);
         return delButton.dataset.key, checkbox.dataset.key, select.dataset.key, textSpan.dataset.key;
     };
-    //reassigns keys to objects and dom elements after deleting a single element
-    locksmith = function (targetKey) {
-        //creates new key from old key
-        var splitKey = targetKey.split("_");
-        var newKeyNum = Number(splitKey[1])+1;
-        var newKey = 'todo_'+newKeyNum;
-        console.log(newKey);
-        //rekey todo
-        
-        //rekey elements
+    //reassigns keys todo objects and dom elements after deleting a single element
+    locksmith = function (deletedKey, newTotal) {
+        //creates new key from old key and checks todos length
+        var i, splitKey, newTargetNum, newKey,
+        splitKey = deletedKey.split("_"),
+        //rekey objects / elements if the deleted todo !== todos length (ie not the last one created)
+        if (splitKey[1] < todoCounter) {
+            //for (i = splitKey, ) {
+                newKey = deletedKey,
+                newTargetNum = Number(splitKey[1])+1,
+                newTarget = 'todo_'+newKeyNum;
+                //rekey todo object
 
-        //update elements id
+                //rekey elements
+
+                //update elements id
+
+            //};
+        };
     };
     //assigns css class based on priority
     casteSystem = function (targetKey, todoPriority){
@@ -109,11 +123,14 @@ var app = (function () {
             element.removeChild(element.firstChild);
             element.remove();
         };
+        console.log("removed elements for "+targetKey);
         todos[targetKey].listed = false;
         //removes todo objects
         deltaco = function (targetKey) {
             delete todos[targetKey];
-            locksmith (targetKey);
+            console.log("counter before re key"+todoCounter);
+            updateCounter();
+            locksmith (targetKey, todoCounter);
         };
         if (reorder === false) {
             deltaco (targetKey);
@@ -137,16 +154,22 @@ var app = (function () {
     //function execution triggered by pressing enter in input field
     inputText.onkeyup = function (event){
         var todo_key, itemText = inputText.value, i, todoText;
-        //if not undefined or one space, proceeds on enter key
+        //if input text value is not undefined or one space, proceed on enter key
+
         if (undefined === itemText || itemText === "" || itemText === " ") {
             return false;
             }
             if (event.which === 13) {
                 // to be added: if end counter is 0 add todo objects from local storage
-                todoCounter = Object.keys(todos).length + 1;
-                todo_key = 'todo_' + todoCounter;
-                console.log(todo_key);
+                updateCounter();
+                console.log("counter before creation: "+todoCounter);
+                keyAssignment = todoCounter + 1;
+                console.log("key assigned: "+keyAssignment);
+                todo_key = 'todo_' + keyAssignment;
+                console.log("created "+todo_key);
                 itsTimeToMakeTheDonuts(todo_key, itemText);
+                //update counter after creation
+                updateCounter();
                 doTheThing(todo_key, todoText);
                 document.getElementById("inputText").value = "";
         };
