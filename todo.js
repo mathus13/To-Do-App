c1b8542fe22344783622074e2f4a9a54fe5fdd4a
HumanSprout <erikj.waters@gmail.com>
2016-03-02 18:47:34 -0500

    almost done

--- todo.js
+++ todo.js
@@ -3,18 +3,16 @@
 var app = (function () {
     var todoCounter,
         todos,
-        inputText = document.getElementById("inputText"),
-        deletedTodos = {};
-    if (localStorage.localCounter && localStorage.localCounter > 0) {
-        console.log("counter found: " + localStorage.localCounter);
-        console.log("todos found:" + localStorage.localTodos)
-        todoCounter = localStorage.localCounter;
-        todos = JSON.parse(localStorage.localTodos);
-        console.log(todos);
+        todosUL = $("ul#todoList"),
+        inputText = $("input:text#inputText");
+    if (localStorage.getItem("localCounter") != undefined) { 
+        todoCounter = Number(localStorage.getItem("localCounter"))
+        todos = JSON.parse(localStorage.getItem("localTodos"));
         reorderElements(true);
     } else {
         console.log("no counter found or counter is 0")
-        todoCounter = 0;
+        todoCounter = 0,
+        todos = {};
     }
     // creates objects
     function createObject(todo_key, itemText) {
@@ -27,46 +25,47 @@ var app = (function () {
     }
     // sets priority value from select and assigns css class
     function setPriority(targetKey, todoPriority, reorder) {
-        var targetSpan = document.getElementById('spanText_' + targetKey),
-            targetLi = document.getElementById('listItem_' + targetKey);
+        var targetSpan = $('span#spanText_' + targetKey),
+            targetLi = $('#listItem_' + targetKey),
+            targetSelect = $('select#select_'+ targetKey);
         // changes priority of selected to-do to value from select
         switch (todoPriority) {
         case '-1':
             todos[targetKey].priority = todoPriority;
-            targetSpan.className = "span_todo-text-completed";
-            targetLi.className = "list-item_default";
+            $(targetSpan).attr("class","span_todo-text-completed");
+            $(targetLi).attr("class","list-item_default");
             if (reorder === true) {
                 reorderElements(false);
             }
             break;
         case '0':
             todos[targetKey].priority = todoPriority;
-            targetSpan.className = "span_todo-text-default";
-            targetLi.className = "list-item_default";
+            $(targetSpan).attr("class","span_todo-text-default");
+            $(targetLi).attr("class","list-item_default");
             if (reorder === true) {
                 reorderElements(false);
             }
             break;
         case '1':
             todos[targetKey].priority = todoPriority;
-            targetSpan.className = "span_todo-text-p1";
-            targetLi.className = "list-item_todo-p1";
+            $(targetSpan).attr("class","span_todo-text-p1");
+            $(targetLi).attr("class","list-item_todo-p1");
             if (reorder === true) {
                 reorderElements(false);
             }
             break;
         case '2':
             todos[targetKey].priority = todoPriority;
-            targetSpan.className = "span_todo-text-p2";
-            targetLi.className = "list-item_todo-p2";
+            $(targetSpan).attr("class","span_todo-text-p2");
+            $(targetLi).attr("class","list-item_todo-p2");
             if (reorder === true) {
                 reorderElements(false);
             }
             break;
         case '3':
             todos[targetKey].priority = todoPriority;
-            targetSpan.className = "span_todo-text-p3";
-            targetLi.className = "list-item_todo-p3";
+            $(targetSpan).attr("class","span_todo-text-p3");
+            $(targetLi).attr("class","list-item_todo-p3");
             if (reorder === true) {
                 reorderElements(false);
             }
@@ -75,66 +74,29 @@ var app = (function () {
             alert("oops you bwoke it");
         }
         if (todos[targetKey].completed === true) {
-            document.getElementById('select_' + targetKey).disabled = true;
+            $(targetSelect).prop("disabled",true);
         } else {
-            document.getElementById('select_' + targetKey).disabled = false;
+            $(targetSelect).prop("disabled",false);
         }
     }
     // creates the DOM elements and assignes properties
     function createElements(todo_key, reorder) {
         // create elements
-        var todoPriority,
-            listItem = document.createElement("li"),
-            delButton = document.createElement("button"),
-            checkbox = document.createElement("input"),
-            select = document.createElement("select"),
-            selectOpt0 = document.createElement("option"),
-            selectOpt1 = document.createElement("option"),
-            selectOpt2 = document.createElement("option"),
-            selectOpt3 = document.createElement("option"),
-            spanText = document.createElement("span");
-        // listItem properties
-        listItem.dataset.key = todo_key;
-        listItem.id = 'listItem_' + todo_key;
-        // (priority level) select properties
-        select.dataset.key = todo_key;
-        select.className = "select";
-        select.id = 'select_' + todo_key;
-        selectOpt0.value = 0;
-        selectOpt0.textContent = 0;
-        selectOpt1.textContent = 1;
-        selectOpt2.textContent = 2;
-        selectOpt3.textContent = 3;
-        // (delete) button properties
-        delButton.dataset.key = todo_key;
-        delButton.className = "button_delete";
-        delButton.id = 'delButton_' + todo_key;
-        // (completed) checkbox properties
-        checkbox.dataset.key = todo_key;
-        checkbox.type = "checkbox";
-        checkbox.className = "checkbox";
-        checkbox.id = 'checkbox_' + todo_key;
-        // (to do text) span properties
-        spanText.dataset.key = todo_key;
-        spanText.id = 'spanText_' + todo_key;
-        spanText.textContent = todos[todo_key].todoText;
-        // append elements
-        todoList.appendChild(listItem);
-        listItem.appendChild(delButton);
-        listItem.appendChild(checkbox);
-        listItem.appendChild(select);
-        select.appendChild(selectOpt0);
-        select.appendChild(selectOpt1);
-        select.appendChild(selectOpt2);
-        select.appendChild(selectOpt3);
-        listItem.appendChild(spanText);
+        var todoPriority, listItem, targetSelect;
+        $(todosUL).append('<li id="listItem_' + todo_key + '" data-key="' + todo_key + '"> </li>');
+        listItem = $("li#listItem_" + todo_key);
+        $(listItem).append('<button class="button_delete" id="delButton_' + todo_key + '" data-key="' + todo_key + '"></button>');
+        $(listItem).append('<input type="checkbox" class="checkbox" id="checkbox_' + todo_key + '" data-key="' + todo_key + '"> </input>');
+        $(listItem).append('<select class="select" id="select_' + todo_key + '" data-key="' + todo_key + '"></select>');
+        targetSelect = $("select#select_" + todo_key);
+        $(targetSelect).append('<option value="0">0</option>').append('<option>1</option>').append('<option>2</option>').append('<option>3</option>');
+        $(listItem).append('<span id="spanText_' + todo_key + '" data-key="' + todo_key + '">' + todos[todo_key].todoText + '</span>');
+        // checks checkbox if item is completed
         if (todos[todo_key].completed === true) {
-            // checks checkbox is extant object is completed
             document.getElementById('checkbox_' + todo_key).checked = true;
         }
         // invoke setPriority to assign span and listItem classes
         todoPriority = todos[todo_key].priority.toString();
-        console.log(reorder);
         setPriority(todo_key, todoPriority, reorder);
     }
     // sets completed property and assigns css class
@@ -180,7 +142,7 @@ var app = (function () {
                     }
                     // recreate elements, pass reorder argument as false to prevent infinite loop
                     createElements(todoByKey, false);
-                    document.getElementById('select_' + todoByKey).value = todos[todoByKey].priority;
+                    $('select#select_' + todoByKey).val(todos[todoByKey].priority)
                 }
             }
         }
@@ -197,6 +159,7 @@ var app = (function () {
         document.getElementById('listItem_' + targetKey).appendChild(inputEdit);
         inputEdit.focus();
     }
+    // removes input field and saves text post edits to object, creates new text span
     function createEditSpan (targetKey, e) {
         todos[targetKey].todoText = e.target.value;
         console.log(e.target.value);
@@ -209,9 +172,9 @@ var app = (function () {
         setPriority(targetKey, todos[targetKey].priority, false);
     }
     // function execution triggered by pressing enter on inputText field
-    inputText.onkeyup = function (event) {
+    $(inputText).keyup(function(e) {
         var todo_key,
-            itemText = inputText.value;
+            itemText = $(inputText).val();
         // if input text value is not undefined or one space, proceed on enter key
         if (undefined === itemText || itemText === "" || itemText === " ") {
             return false;
@@ -221,47 +184,50 @@ var app = (function () {
             todo_key = 'todo_' + todoCounter;
             createObject(todo_key, itemText);
             createElements(todo_key, false);
-            document.getElementById("inputText").value = "";
+            $(inputText).val("");
         }
-    };
+    });
     // delete button event listener on click
-    document.getElementById("todoList").addEventListener("click", function (e) {
+    $(todosUL).click(function(e) {
         if (e.target.className === "button_delete") {
             console.log("del button clicked " + e.target.dataset.key);
             removeElements(e.target.dataset.key, false);
         }
     });
     // check box event listener on click
-    document.getElementById("todoList").addEventListener("click", function (e) {
+    $(todosUL).click(function(e) {
         if (e.target.className === "checkbox") {
             console.log("checkbox clicked: " + e.target.dataset.key);
             setCompleted(e.target.dataset.key, e.target.checked);
         }
     });
     // select event listener on change
-    document.getElementById("todoList").addEventListener("change", function (e) {
+    $(todosUL).change(function(e) {
         if (e.target.className === "select") {
             console.log("select clicked: " + e.target.dataset.key);
             setPriority(e.target.dataset.key, e.target.value, true);
         }
     });
     // span event listener on double click
-    document.getElementById("todoList").addEventListener("dblclick", function (e) {
-        if (e.target.className != "span_todo-text-completed") {
+    $(todosUL).dblclick(function(e) {
+        if (e.target.className === "span_todo-text-default"|| e.target.className === "span_todo-text-p1" || e.target.className === "span_todo-text-p2" || e.target.className === "span_todo-text-p3") {
             console.log("spanText clicked: " + e.target.dataset.key);
             createEditInput(e.target.dataset.key);
         }
     });
-    document.getElementById("todoList").addEventListener("keyup", function (e) {
+    // input field event listener on keyup
+    $(todosUL).keyup(function(e) {
         if (event.which === 13 && e.target.className === "inputEdit") {
             e.target.blur();
         }
     });
-    document.getElementById("todoList").addEventListener("focusout", function (e) {
+    // input field event listener on focusout
+    $(todosUL).focusout(function(e) {
         if (e.target.className === "inputEdit") {
             createEditSpan(e.target.dataset.key, e);
         }
     });
+    // automatigically saves counter val and todos {} to localStorage on window close / refresh
     window.addEventListener("unload", function (e) {
         localStorage.setItem('localTodos', JSON.stringify(todos));
         localStorage.setItem('localCounter', todoCounter);
